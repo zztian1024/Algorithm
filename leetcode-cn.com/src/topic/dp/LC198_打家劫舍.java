f6f0a422ba36b75b9a1d6bf47feb4516b8734683
zhenzi <tianzhenzi@inke.cn>
2021-04-17 22:30:18 +0800

    Create LC198_打家劫舍.java

---
+++ leetcode-cn.com/src/topic/dp/LC198_打家劫舍.java
@@ -0,0 +1,21 @@
+package topic.dp;
+
+public class LC198_打家劫舍 {
+	public int rob(int[] nums) {
+		// [1,2,3,1]
+		// 动态规划
+		if (nums == null || nums.length == 0)
+			return 0;
+		if (nums.length == 1)
+			return nums[0];
+
+		int[] dp = new int[nums.length + 1];
+		dp[0] = nums[0];
+		dp[1] = Math.max(dp[0], nums[1]);
+		for (int i = 2; i < nums.length; i++) {
+			dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+		}
+
+		return dp[nums.length - 1];
+	}
+}

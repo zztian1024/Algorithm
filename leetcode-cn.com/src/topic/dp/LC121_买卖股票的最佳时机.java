0fd1965a57d407e3aac49f9c27fbf906ca9389ba
zhenzi <tianzhenzi@inke.cn>
2021-04-17 20:04:48 +0800

    Create LC121_买卖股票的最佳时机.java

---
+++ leetcode-cn.com/src/topic/dp/LC121_买卖股票的最佳时机.java
@@ -0,0 +1,16 @@
+package topic.dp;
+
+public class LC121_买卖股票的最佳时机 {
+	public int maxProfit(int[] prices) {
+		int minprice = Integer.MAX_VALUE;
+		int maxprofit = 0;
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] < minprice) {
+				minprice = prices[i];
+			} else if (prices[i] - minprice > maxprofit) {
+				maxprofit = prices[i] - minprice;
+			}
+		}
+		return maxprofit;
+	}
+}

fee5a03ff0978d77aa31fcf6167d87d5dea94237
zhenzi <tianzhenzi@inke.cn>
2021-04-18 20:58:00 +0800

    Create LC144_二叉树的前序遍历.java

---
+++ leetcode-cn.com/src/topic/binarytree/LC144_二叉树的前序遍历.java
@@ -0,0 +1,36 @@
+package topic.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import base.TreeNode;
+
+public class LC144_二叉树的前序遍历 {
+	private List<Integer> list = new ArrayList<>();
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+		if (root == null) {
+			return list;
+		}
+		inorderTraversal(root.left);
+		list.add(root.val);
+		inorderTraversal(root.right);
+		return list;
+	}
+
+	// -------------------------
+	public List<Integer> inorderTraversal2(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		inorder(root, res);
+		return res;
+	}
+
+	public void inorder(TreeNode root, List<Integer> res) {
+		if (root == null) {
+			return;
+		}
+		inorder(root.left, res);
+		res.add(root.val);
+		inorder(root.right, res);
+	}
+}

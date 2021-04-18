d2dad46455c3c18da29b1d21ad82c315108103e4
zhenzi <tianzhenzi@inke.cn>
2021-04-18 21:44:52 +0800

    Create LC226_翻转二叉树.java

---
+++ leetcode-cn.com/src/topic/binarytree/LC226_翻转二叉树.java
@@ -0,0 +1,16 @@
+package topic.binarytree;
+
+import base.TreeNode;
+
+public class LC226_翻转二叉树 {
+	public TreeNode invertTree(TreeNode root) {
+		if (root == null) {
+			return null;
+		}
+		TreeNode left = invertTree(root.left);
+		TreeNode right = invertTree(root.right);
+		root.left = right;
+		root.right = left;
+		return root;
+	}
+}

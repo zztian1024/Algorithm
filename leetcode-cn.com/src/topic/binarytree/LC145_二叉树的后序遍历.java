ba885b1dc9482dac4e954d73c70e9ca3227b6354
zhenzi <tianzhenzi@inke.cn>
2021-04-20 00:12:15 +0800

    Update LC145_二叉树的后序遍历.java

--- leetcode-cn.com/src/topic/binarytree/LC145_二叉树的后序遍历.java
+++ leetcode-cn.com/src/topic/binarytree/LC145_二叉树的后序遍历.java
@@ -7,17 +7,18 @@ import base.TreeNode;
 
 public class LC145_二叉树的后序遍历 {
 	public List<Integer> postorderTraversal(TreeNode root) {
-	    List<Integer> res = new ArrayList<>();
-	    postorder(root, res);
-	    return res;
+		List<Integer> res = new ArrayList<>();
+		postorder(root, res);
+		return res;
 	}
+
 	void postorder(TreeNode root, List<Integer> list) {
-	    if (root == null) {
-	        return;
-	    }
-	    postorder(root.left, list);
-	    postorder(root.right, list);
-	    list.add(root.val);
+		if (root == null) {
+			return;
+		}
+		postorder(root.left, list);
+		postorder(root.right, list);
+		list.add(root.val);
 	}
 }
 

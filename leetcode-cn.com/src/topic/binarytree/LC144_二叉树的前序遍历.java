608bd5aa235a94c9b83d30697c42874b0b0150a9
zhenzi <tianzhenzi@inke.cn>
2021-04-19 09:10:24 +0800

    Update LC144_二叉树的前序遍历.java

--- leetcode-cn.com/src/topic/binarytree/LC144_二叉树的前序遍历.java
+++ leetcode-cn.com/src/topic/binarytree/LC144_二叉树的前序遍历.java
@@ -2,35 +2,46 @@ package topic.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import base.TreeNode;
 
 public class LC144_二叉树的前序遍历 {
-	private List<Integer> list = new ArrayList<>();
 
-	public List<Integer> inorderTraversal(TreeNode root) {
-		if (root == null) {
-			return list;
-		}
-		inorderTraversal(root.left);
-		list.add(root.val);
-		inorderTraversal(root.right);
-		return list;
+	// 递归解法-------------------------
+	ArrayList<Integer> preOrderReverse(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		preOrder2(root, result);
+		return result;
 	}
 
-	// -------------------------
-	public List<Integer> inorderTraversal2(TreeNode root) {
-		List<Integer> res = new ArrayList<Integer>();
-		inorder(root, res);
-		return res;
-	}
-
-	public void inorder(TreeNode root, List<Integer> res) {
+	void preOrder2(TreeNode root, ArrayList<Integer> result) {
 		if (root == null) {
 			return;
 		}
-		inorder(root.left, res);
-		res.add(root.val);
-		inorder(root.right, res);
+		result.add(root.val);
+		preOrder2(root.left, result);
+		preOrder2(root.right, result);
+	}
+
+	// 迭代解法-------------------------
+	ArrayList<Integer> preOrder(TreeNode root) {
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		if (root == null) {
+			return list;
+		}
+		stack.push(root);
+		while (!stack.empty()) {
+			TreeNode node = stack.pop();
+			list.add(node.val);
+			if (node.right != null) {
+				stack.push(node.right);
+			}
+			if (node.left != null) {
+				stack.push(node.left);
+			}
+		}
+		return list;
 	}
 }

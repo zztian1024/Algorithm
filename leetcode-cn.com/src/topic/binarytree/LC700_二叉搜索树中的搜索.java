b342fae81b6f00959502aa85c90e30939cfb9a59
zhenzi <tianzhenzi@inke.cn>
2021-04-20 08:47:10 +0800

    Create LC700_二叉搜索树中的搜索.java

---
+++ leetcode-cn.com/src/topic/binarytree/LC700_二叉搜索树中的搜索.java
@@ -0,0 +1,17 @@
+package topic.binarytree;
+
+import base.TreeNode;
+
+public class LC700_二叉搜索树中的搜索 {
+	public TreeNode searchBST(TreeNode root, int val) {
+		if (root == null) {
+			return null;
+		}
+		if (root.val == val)
+			return root;
+		if (root.val < val)
+			return searchBST(root.right, val);
+		else
+			return searchBST(root.left, val);
+	}
+}

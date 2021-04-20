1b681e0c56c9822738825e25b400ebe476da3716
zhenzi <tianzhenzi@inke.cn>
2021-04-20 23:23:15 +0800

    Update LC070_爬楼梯.java

--- leetcode-cn.com/src/topic/dp/LC070_爬楼梯.java
+++ leetcode-cn.com/src/topic/dp/LC070_爬楼梯.java
@@ -10,4 +10,14 @@ public class LC070_爬楼梯 {
 		}
 		return c;
 	}
+
+	public int climbStairs2(int n) {
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[n];
+	}
 }

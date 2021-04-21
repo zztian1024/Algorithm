28288165045770af9d26bff893c7c683acfc184a
zhenzi <tianzhenzi@inke.cn>
2021-04-21 08:34:02 +0800

    Update LC160_相交链表.java

--- leetcode-cn.com/src/topic/linkedlist/LC160_相交链表.java
+++ leetcode-cn.com/src/topic/linkedlist/LC160_相交链表.java
@@ -6,6 +6,17 @@ import base.ListNode;
 
 public class LC160_相交链表 {
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if (headA == null || headB == null)
+			return null;
+		ListNode l1 = headA, l2 = headB;
+		while (l1 != l2) {
+			l1 = l1 == null ? headB : l1.next;
+			l2 = l2 == null ? headA : l2.next;
+		}
+		return l1;
+	}
+
+	public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
 		HashSet<ListNode> set = new HashSet<>();
 		ListNode l1 = headA, l2 = headB;
 		while (l1 != null) {

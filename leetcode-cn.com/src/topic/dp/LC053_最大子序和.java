d1a6ccde3521703840e2ea9b01816384be0d97b1
zhenzi <tianzhenzi@inke.cn>
2021-04-21 08:34:10 +0800

    Update LC053_最大子序和.java

--- leetcode-cn.com/src/topic/dp/LC053_最大子序和.java
+++ leetcode-cn.com/src/topic/dp/LC053_最大子序和.java
@@ -2,17 +2,29 @@ package topic.dp;
 
 public class LC053_最大子序和 {
 	public int maxSubArray(int[] nums) {
+		// 如果是一个元素直接返回
+		int len = nums.length;
+		if (len == 1) {
+			return nums[0];
+		}
+		int dp = nums[0], res = dp;
+		for (int i = 1; i < len; i++) {
+			dp = Math.max(nums[i], nums[i] + dp);
+			res = Math.max(dp, res);
+		}
+		return res;
+	}
+
+	public int maxSubArray1(int[] nums) {
 		int len = nums.length;
 		if (len == 1) {
 			return nums[0];
 		}
 		int[] dp = new int[len];
 		dp[0] = nums[0];
+		int res = dp[0];
 		for (int i = 1; i < len; i++) {
 			dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
-		}
-		int res = Integer.MIN_VALUE;
-		for (int i = 0; i < len; i++) {
 			res = Math.max(res, dp[i]);
 		}
 		return res;

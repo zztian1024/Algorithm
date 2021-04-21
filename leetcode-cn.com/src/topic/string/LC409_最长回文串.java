5c64ed995f9a3c1d9a295a01765c5d1e76d55e7d
zhenzi <tianzhenzi@inke.cn>
2021-04-21 08:33:56 +0800

    Create LC409_最长回文串.java

---
+++ leetcode-cn.com/src/topic/string/LC409_最长回文串.java
@@ -0,0 +1,21 @@
+package topic.string;
+
+public class LC409_最长回文串 {
+	public int longestPalindrome(String s) {
+		int[] count = new int[128];
+		int length = s.length();
+		for (int i = 0; i < length; ++i) {
+			char c = s.charAt(i);
+			count[c]++;
+		}
+
+		int ans = 0;
+		for (int v : count) {
+			ans += v / 2 * 2;
+			if (v % 2 == 1 && ans % 2 == 0) {
+				ans++;
+			}
+		}
+		return ans;
+	}
+}

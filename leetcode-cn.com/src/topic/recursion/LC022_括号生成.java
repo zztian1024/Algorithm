af46e8ca6442619aa2c59bb78ce97ba95101e179
zhenzi <tianzhenzi@inke.cn>
2021-04-21 23:38:06 +0800

    Create LC022_括号生成.java

---
+++ leetcode-cn.com/src/topic/recursion/LC022_括号生成.java
@@ -0,0 +1,32 @@
+package topic.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC022_括号生成 {
+	public List<String> generateParenthesis(int n) {
+		List<String> result = new ArrayList<>();
+		_generate(0, 0, n, "", result);
+		return result;
+	}
+
+	void _generate(int left, int right, int max, String str, List<String> result) {
+		// 终止条件
+		if (left == max && right == max) {
+			// 处理结果
+			result.add(str);
+			return;
+		}
+
+		// 处理当前
+
+		// 下转
+		// 剪枝
+		if (left < max)
+			_generate(left + 1, right, max, str + "(", result);
+		if (right < left)
+			_generate(left, right + 1, max, str + ")", result);
+
+		// 清理当前状态
+	}
+}
